5e37e7e1cd2d59e1d1c70a4ad0e0d9fede83bdc5
Ivy Patton <lilaivy@gmail.com>
2017-05-03 16:28:31 -0700

    it roundtrips a brewery

--- test/e2e/breweries.test.js
+++ test/e2e/breweries.test.js
@@ -15,5 +15,45 @@ describe('breweries api', () => {
             });
     });
 
+    let fakeBrewery1 = {
+        name: 'Breakside',
+        neighborhood: 'Woodlawn',
+        specialties: ['pilsner', 'ipa', 'seasonal'],
+    };
+    // let fakeBrewery2 = {
+    //     name: 'Cascade',
+    //     neighborhood: 'Buckman',
+    //     specialties: ['sours', 'seasonal'],
+    // };
+    // let fakeBrewery3 = {
+    //     name: 'Commons',
+    //     neighborhood: 'Central SE',
+    //     specialties: ['farmhouse', 'saizon'],
+    // };
+
+    function saveBrewery(brewery) {
+        return request
+            .post('/api/breweries')
+            .send(brewery)
+            .then(res => res.body);
+    }
+
+    it('roundtrips a new brewery', () => {
+        return saveBrewery(fakeBrewery1)
+            .then(savedBrewery => {
+                assert.ok(savedBrewery._id, 'saved brewery has an id');
+                fakeBrewery1 = savedBrewery;
+            })
+            .then(() => {
+                return request.get(`/api/breweries/${fakeBrewery1._id}`);
+            })
+            .then(res => res.body)
+            .then(gotBrewery => {
+                assert.deepEqual(gotBrewery, fakeBrewery1);
+            });
+    });
+
+
+
 
 });
