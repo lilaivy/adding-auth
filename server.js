eeac8a785e51e2db080b4a6aa9896a14f98c3150
Ivy Patton <lilaivy@gmail.com>
2017-06-21 18:14:45 -0700

    added connection string to server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 /* eslint no-console: "off"*/
 const app = require('./lib/app');
 const http = require('http');
-require('./lib/connect');
+
+const connect = require('./lib/connect');
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/breweries';
+connect(dbUri);
 
 const server = http.createServer(app);
 
